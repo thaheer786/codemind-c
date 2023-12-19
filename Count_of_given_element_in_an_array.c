903536c7bc5d679f9abf61a0a2a789ac20068b54
thaheer786 <147476099+thaheer786@users.noreply.github.com>
2023-12-19 09:51:49 +0530

    #include<stdio.h> int main() {     int n,i,c=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int m;     scanf("%d",&m);     for(i=0;i<n;i++)     {         if(arr[i]==m)         c++;     }     printf("%d",c); }

---
+++ Count_of_given_element_in_an_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,c=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int m;
+    scanf("%d",&m);
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]==m)
+        c++;
+    }
+    printf("%d",c);
+}
