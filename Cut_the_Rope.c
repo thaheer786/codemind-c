5b518267214d7a5e58a9ce39c75cde6821bba038
thaheer786 <147476099+thaheer786@users.noreply.github.com>
2024-01-18 19:22:08 +0530

    #include<stdio.h> int main() {     int a,b,c;     scanf("%d%d",&a,&b);     c=a-b;     if(b%2==0&&c%2==0)     printf("YES");     else if(b%2!=0 && c%2!=0)     printf("YES");     else if(b%2!=0 && c%2==0)     printf("YES");     else     printf("NO");
    }

---
+++ Cut_the_Rope.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=a-b;
+    if(b%2==0&&c%2==0)
+    printf("YES");
+    else if(b%2!=0 && c%2!=0)
+    printf("YES");
+    else if(b%2!=0 && c%2==0)
+    printf("YES");
+    else
+    printf("NO");
+    
+}
